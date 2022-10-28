0db64c49519f0fef4eddf2c0ab7d6d5efe1a5aa5
raugustinas <augustinas.rokas@gmail.com>
2022-10-28 13:14:44 +0300

    Text component added

---
+++ src/components/text/index.tsx
@@ -0,0 +1,19 @@
+import React, {FC} from 'react';
+import {Text as RNText, TextProps as RNTextProps} from 'react-native';
+import styled from 'styled-components/native';
+import theme, {Color, Typography} from '@/utils/theme';
+
+const TextStyled = styled(RNText)<{color: Color}>`
+  color: ${({color, theme: {colors}}) => colors[color]};
+`;
+
+interface TextProps extends Partial<RNTextProps> {
+  type: Typography;
+  color: Color;
+}
+
+const Text: FC<TextProps> = ({type, style, ...props}, ref) => (
+  <TextStyled ref={ref} style={[theme?.typography?.[type], style]} {...props} />
+);
+
+export default Text;
