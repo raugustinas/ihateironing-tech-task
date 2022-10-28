0935898346245d03c955d94b64da410f78971956
raugustinas <augustinas.rokas@gmail.com>
2022-10-28 17:00:49 +0300

    Details screen added

--- src/screens/details/index.tsx
+++ src/screens/details/index.tsx
@@ -1,13 +1,52 @@
 import React, {FC} from 'react';
+import {useDispatch} from 'react-redux';
+import {Alert, ScrollView} from 'react-native';
 import styled from 'styled-components/native';
+import FastImage from 'react-native-fast-image';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Item} from '@/types/types';
+import Text from '@/components/text';
 import {useFetch} from '@/hooks/useFetch';
 import Loading from '@/components/loading';
+import {Button} from '@/components/button';
 import {DefaultNavigationProps} from '@/types/navigation';
 
-const Container = styled(SafeAreaView)`
+const Container = styled(SafeAreaView).attrs({
+  edges: ['bottom'],
+})`
   flex: 1;
+`;
+
+const ScrollViewStyled = styled(ScrollView)`
+  flex: 1;
+`;
+
+const ItemImage = styled(FastImage).attrs({
+  resizeMode: FastImage.resizeMode.cover,
+})`
+  width: 100%;
+  aspect-ratio: 1.5;
+`;
+
+const BodyContainer = styled.View`
+  flex: 1;
+  padding: 15px;
+`;
+
+const TitleText = styled(Text).attrs({
+  type: 'h1',
+  color: 'black',
+})``;
+
+const PriceText = styled(Text).attrs({
+  type: 'h1',
+  color: 'primary',
+})`
+  margin-top: 2px;
+`;
+
+const FooterContainer = styled.View`
+  padding: 15px;
 `;
 
 interface Props {
@@ -15,14 +54,33 @@ interface Props {
   navigation: DefaultNavigationProps<'Details'>;
 }
 
-const DetailsScreen: FC<Props> = ({navigation, id}) => {
+const DetailsScreen: FC<Props> = ({id}) => {
+  const {cart} = useDispatch();
   const {data, loading} = useFetch<Item>(`/items/${id}`);
+
+  const handleBuyPress = () => {
+    cart.addToCart(data);
+    Alert.alert('Thank you!', 'Item added to cart!');
+  };
 
   if (loading) {
     return <Loading />;
   }
 
-  return <Container />;
+  return (
+    <Container>
+      <ScrollViewStyled>
+        <ItemImage source={{uri: data?.image}} />
+        <BodyContainer>
+          <TitleText>{data?.name}</TitleText>
+          <PriceText>{`$${data?.price}`}</PriceText>
+        </BodyContainer>
+      </ScrollViewStyled>
+      <FooterContainer>
+        <Button onPress={handleBuyPress}>BUY</Button>
+      </FooterContainer>
+    </Container>
+  );
 };
 
 export default DetailsScreen;
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -3,4 +3,5 @@ export type Item = {
   price?: number;
   name?: string;
   image?: string;
+  quantity?: number;
 };
